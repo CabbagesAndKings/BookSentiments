03e025b97f4b36aa03bb5ca68f0f457d2883ae7d
ninjasnowman <eeshan@gmail.com>
2013-07-30 14:43:21 +0530

    code cleanup 2

--- scripts/visualize_book_sentiments.R
+++ scripts/visualize_book_sentiments.R
@@ -1,5 +1,5 @@
 
-setwd('C:/etc/Projects/Data/_Ongoing/BookSentiment')
+setwd('C:/etc/Projects/Data/_Complete/BookSentiment')
 library(ggplot2)
 library(reshape2)
 
@@ -14,7 +14,10 @@ book.file <- paste0("data/",book.label,".txt")
 #Initialize sentiment dictionary
 df.sentiments <- read.table(sentiment.file,header=F,sep="\t",quote="",col.names=c("term","score"))
 df.sentiments$term <- gsub("[^[:alnum:]]", " ",df.sentiments$term)
+
 #row.names(df.sentiments) <- df.sentiments$term
+#Initially tried the lookup using row names, but that also finds partial matches. 
+#Anyone know of a way to turn partial matching off?
 
 ScoreTerm <- function(term){
 	df.sentiments[match(term,df.sentiments[,"term"]),"score"]
@@ -59,24 +62,25 @@ percent.scores <- as.data.frame(sapply(scores,RollUpScores))
 colnames(percent.scores)<-book.label.list
 percent.scores$percent <- 1:nrow(percent.scores)
 
-escores <- melt(percent.scores[,c(1,8)],"percent",book.label.list[1],variable.name="book",value.name="sentiment")
-g0<- ggplot(escores, aes(x = percent, y = sentiment, color=factor(book)))
+escores <- melt(percent.scores,"percent",book.label.list,variable.name="book",value.name="sentiment")
 
-g0 + geom_point() + stat_smooth(method="loess",span=0.5) + geom_hline() + facet_grid(book ~.) + theme(legend.position="none")
-g0 + stat_smooth(method="loess") + geom_hline() +  theme(legend.position="bottom")
+plot.sentiment<- ggplot(escores, aes(x = percent, y = sentiment, color=factor(book)))
+
+plot.sentiment + geom_point() + stat_smooth(method="loess",span=0.5) + geom_hline() + facet_grid(book ~.) + theme(legend.position="none")
+plot.sentiment + stat_smooth(method="loess") + geom_hline() +  theme(legend.position="bottom")
 
 
 
-g <- ggplot(escores, aes(x = percent, y = 0))
-g1 <- g + geom_tile(aes(fill = sentiment)) +
+plot.sentiment.heatmap <- ggplot(escores, aes(x = percent, y = 0))
+plot.sentiment.heatmap.2 <- plot.sentiment.heatmap + geom_tile(aes(fill = sentiment)) +
 	  	scale_fill_gradient2(low='red',mid="yellow", high='green') +
 	  	theme(legend.position="none") + 
 	  	xlab("Part") + ylab("") + 
 	  	ggtitle("Sentiment Heatmap of the Book\n") +
 		scale_x_continuous(expand = c(0,0)) +
 		scale_y_continuous(expand = c(0,0))
-g1
-g1 + theme(axis.text.y = element_blank(), axis.ticks.y = element_blank()) 
+plot.sentiment.heatmap.2
+plot.sentiment.heatmap.2 + theme(axis.text.y = element_blank(), axis.ticks.y = element_blank()) 
 
 
 
